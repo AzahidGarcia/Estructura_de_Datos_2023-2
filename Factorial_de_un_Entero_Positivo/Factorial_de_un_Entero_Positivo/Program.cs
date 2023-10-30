1aaeb8743aab4cd764be0c0a9d5aa5a9790ae94a
Azahid Garcia <92194706+AzahidGarcia@users.noreply.github.com>
2023-10-30 21:04:25 +0100

    Ejercicio 1 Semana 1

---
+++ Factorial_de_un_Entero_Positivo/Factorial_de_un_Entero_Positivo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, ingrese un número entero positivo: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int numero) && numero >= 0)
+        {
+            long factorial = CalcularFactorial(numero);
+            Console.WriteLine($"El factorial de {numero} es {factorial}");
+        }
+        else
+        {
+            Console.WriteLine("El número ingresado no es un entero positivo válido.");
+        }
+    }
+
+    static long CalcularFactorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
