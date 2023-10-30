67125f5a91422cbe4de1d55090fb38a4f4c5a01c
Azahid Garcia <92194706+AzahidGarcia@users.noreply.github.com>
2023-10-30 20:51:45 +0100

    Ejercicio 2 semana 2

---
+++ Ejercicio2/Ejercicio2/Program2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Crear una pila fuente
+        Stack<int> pilaFuente = new Stack<int>();
+        pilaFuente.Push(1);
+        pilaFuente.Push(2);
+        pilaFuente.Push(3);
+
+        // Crear una pila destino
+        Stack<int> pilaDestino = new Stack<int>();
+
+        // Llamar al método para copiar la pila
+        CopiarPila(pilaFuente, pilaDestino);
+
+        // Imprimir la pila destino para verificar la copia
+        Console.WriteLine("Pila Destino (copia de la pila fuente):");
+        foreach (int elemento in pilaDestino)
+        {
+            Console.WriteLine(elemento);
+        }
+    }
+
+    static void CopiarPila<T>(Stack<T> pilaFuente, Stack<T> pilaDestino)
+    {
+        // Crear una pila temporal para mantener el orden correcto
+        Stack<T> pilaTemporal = new Stack<T>();
+
+        // Copiar elementos de la pila fuente a la pila temporal
+        foreach (T elemento in pilaFuente)
+        {
+            pilaTemporal.Push(elemento);
+        }
+
+        // Vaciar la pila destino (por si tenía elementos previos)
+        pilaDestino.Clear();
+
+        // Copiar elementos de la pila temporal a la pila destino
+        foreach (T elemento in pilaTemporal)
+        {
+            pilaDestino.Push(elemento);
+        }
+    }
+}
